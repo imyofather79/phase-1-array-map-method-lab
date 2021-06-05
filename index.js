5709e15465e83788f2751c0cfc24156fb813fa34
imyofather79 <chunsum708@gmail.com>
2021-06-04 18:38:53 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -12,5 +12,9 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => {
+    let str = tutorial.split(" ");
+    const newArray = str.map(word => word[0].toUpperCase() + word.slice(1))
+    return newArray.join(" ");
+  })
 }
